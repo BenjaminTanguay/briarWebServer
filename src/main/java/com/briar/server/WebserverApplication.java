95ccd0f2108e9871d72b55771f76eeafb59b9b4c
Benjamin T <Benjamin.tanguay@gmail.com>
2018-03-02 03:02:38 -0500

    Allowed the app to properly access all Mappers created for Batis

--- src/main/java/com/briar/server/WebserverApplication.java
+++ src/main/java/com/briar/server/WebserverApplication.java
@@ -1,13 +1,14 @@
 package com.briar.server;
 
-import com.briar.server.model.User;
+import com.briar.server.model.domainmodelclasses.User;
+import com.briar.server.model.domainmodelclasses.UserContact;
 import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@MappedTypes(User.class)
+@MappedTypes({User.class, UserContact.class})
 @MapperScan("com.briar.server.mapper")
 @ComponentScan(basePackages = {"com.briar.server.config", "com.briar.server.resources"})
 @SpringBootApplication
